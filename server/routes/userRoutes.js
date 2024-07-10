6a91c86de16da0e2b95780fe70cd9629e23cc4f7
Manan21st <manan9346@gmail.com>
2024-07-10 10:45:15 +0530

    user created

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,12 +4,28 @@ const User = require("../models/userModel");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+    try{
 
+        const UserExists = await User.findOne({email:req.body.email});
+        if(UserExists){
+            res.send({
+                success:false,
+                message:"User already exists"
+            });
+        }
+
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.status(201).json("User has been registered successfully");
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
 
 });
 
 router.post("/login", async (req, res) => {
-  
+
 });
 
 
