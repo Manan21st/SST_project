1648961fdc4bb03710a91d2702bfab7f8b877d88
Manan21st <manan9346@gmail.com>
2024-07-13 10:47:55 +0530

    protectedroute implemented

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 function App() {
@@ -12,14 +13,13 @@ function App() {
 
   return (
     <div className="App">
-     
       <BrowserRouter>
         <Routes>
     
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home/>} />
-         
+          <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
+
         </Routes>
       </BrowserRouter>
     </div>
